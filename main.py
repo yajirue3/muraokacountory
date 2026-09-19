880af60fe76b0ea908a3759d97265f390951de9f
yajirue3 <yajirue3@gmail.com>
2026-09-19 15:04:32 +0900

    Add video router to main application

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ from factory import router as factory_router
 from card import router as card_router
 from db import get_supabase
 from game_2048 import router as game_2048_router
-from video import router as video_router
 
 SUPABASE_URL = os.environ.get("SUPABASE_URL", "")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "")
@@ -614,6 +613,7 @@ app.include_router(policy_router)
 app.include_router(factory_router)
 app.include_router(card_router)
 app.include_router(game_2048_router)
+from video import router as video_router
 app.include_router(video_router)
 
 # ==================================================
